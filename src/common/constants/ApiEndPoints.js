5f95d32c3494310da2e83b64241cfbc89540ded6
jonniebigodes <jonniebigodes@hotmail.com>
2018-01-14 23:20:39 +0000

    Alterados api endpoints para servidor remoto

--- src/common/constants/ApiEndPoints.js
+++ src/common/constants/ApiEndPoints.js
@@ -1,28 +1,28 @@
 //endpoints facturas
 export const localGetFaturas='http://localhost:8090/api/faturas/getfaturas';
-export const externalGetFaturas='http://localhost:5000/api/data/nighttoken';
+export const externalGetFaturas='https://csmserver.herokuapp.com/api/faturas/getfaturas';
 //
 //endpoints recibos
 export const localGetRecibos='http://localhost:8090/api/recibos/getrecibos';
-export const externalGetRecibos='http://localhost:5000/api/data/nighttoken';
+export const externalGetRecibos='https://csmserver.herokuapp.com/api/recibos/getrecibos';
 //
 // endpoints clientes
 export const localGetClientes='http://localhost:8090/api/clientes/getclientes';
-export const externalGetClientes='http://localhost:5000/api/data/nighttoken'; 
+export const externalGetClientes='https://csmserver.herokuapp.com/api/clientes/getclientes'; 
 //
 //endpoints funcionarios
 export const localGetFuncionarios='http://localhost:8090/api/funcionarios/getfuncionarios';
-export const externalGetFuncionarios='http://localhost:5000/api/data/nighttoken'; 
+export const externalGetFuncionarios='https://csmserver.herokuapp.com/api/funcionarios/getfuncionarios'; 
 
 //
 //endpoints ocorrencias
 export const localGetOcorrencias='http://localhost:8090/api/ocorrencias/getocorrencias';
-export const externalGetOcorrencias='http://localhost:5000/api/data/nighttoken'; 
+export const externalGetOcorrencias='https://csmserver.herokuapp.com/api/ocorrencias/getocorrencias'; 
 //
 
 //endpoints tipo servicos
 export const localGetTiposServico='http://localhost:8090/api/servicos/gettipos';
-export const externalGetTiposServico='http://localhost:5000/api/data/nighttoken';
+export const externalGetTiposServico='https://csmserver.herokuapp.com/api/servicos/gettipos';
 //
 
 // endpoint todos os dados
